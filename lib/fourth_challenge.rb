77f1c398863260b4678dedf8c8ff7587ac9c9226
Amy Fogelman <abfnj1@gmail.com>
2019-06-07 00:37:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,8 +4,8 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [name: "Benvolio", age: "17", attitude: "worried"]
-      
+      :hero_best_friend => [name: "Benvolio", age: "17", attitude: "worried"]
+      :hero_second_friend => [name: "Mercutio", age: "18", attitude: "hothead"]
       
         #your hashes here!
 
